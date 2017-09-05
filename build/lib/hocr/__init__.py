dd10f7ab28078d682433eb59da67fad32830c94b
Rune Loyning <rune@loyning.net>
2017-09-05 18:43:56 +0200

    add installation file setup.py

--- build/lib/hocr/__init__.py
+++ build/lib/hocr/__init__.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+# from .parser import parse
+
 __title__ = 'hocr'
 __version__ = '0.2.12'
 __copyright__ = 'Copyright 2017 Vic.ai'
---
+++ setup.py
@@ -0,0 +1,29 @@
+#
+# Copyright 2017 Vic.ai - Rune Loyning
+#
+# https://github.com/Vic-ai/python-hocr
+#
+
+from setuptools import find_packages
+from distutils.core import setup
+from pkgutil import get_importer
+
+meta = get_importer('hocr').find_module('__init__').load_module('__init__')
+
+setup(
+    name="hocr",
+    version=meta.__version__,
+    description=meta.__description__,
+    author='Vic.ai',
+    author_email='rune@vic.ai',
+    url='http://github.com/loyning/python-24so/',
+    keywords='hocr',
+    classifiers=[],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'beautifulsoup4',
+        'six',
+        'lxml'
+    ],
+)
