a830ffcc28b953cf04331f82695df1d19cb7bf43
algorythmik <smt.moji@gmail.com>
2018-05-09 13:17:16 +0200

    Update Info.

--- pyhocr/__init__.py
+++ pyhocr/__init__.py
@@ -3,6 +3,5 @@
 __title__ = 'pyhocr'
 __version__ = '1.0.0'
 __copyright__ = 'Copyright 2017 Vic.ai'
-__description__ = """HOCR manipulation and utility library, based on @jsfenfen
-                     and @concordusapps"""
+__description__ = """HOCR manipulation and utility library"""
 __author__ = 'rune@vic.ai'
