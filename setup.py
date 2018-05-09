c207d7e6063caa4ec463094766ebca48252ae446
Mojtaba Tabatabaeipour <smt.moji@gmail.com>
2018-05-09 11:39:22 +0200

    Bump version: 0.2.13 → 1.0.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup
 from pkgutil import get_importer
 
 meta = get_importer('pyhocr').find_module('__init__').load_module('__init__')
-__version__ = '0.2.13'
+__version__ = '1.0.0'
 
 setup(
     name="pyhocr",
